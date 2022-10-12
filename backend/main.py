551cf832e989b2e21fee637cf19f3d274879ee23
Joe Ciesielski <jtcies@gmail.com>
2022-10-11 20:33:03 -0400

    adding middleware

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,23 @@ from fastapi import FastAPI
 from opensky_api import OpenSkyApi
 from dotenv import load_dotenv
 import os
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 OPENSKY_USER = os.environ.get("OPENSKY_USER")
 OPENSKY_PASSWORD = os.environ.get("OPENSKY_PASSWORD")
 
 app = FastAPI()
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root(lon: float, lat: float, range: float = 0.25):
